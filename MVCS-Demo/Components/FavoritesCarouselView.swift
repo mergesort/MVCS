edb72d3b3315d49c78afd7719decdf87574d9a34
Joe Fabisevich <joe@fabisevi.ch>
2022-06-23 00:20:40 -0400

    Updating FavoritesCarouselView to use onReceive rather than processing imagesController.images directly
    Also adding documentation explaining why this is the best approach.

--- MVCS-Demo/Components/FavoritesCarouselView.swift
+++ MVCS-Demo/Components/FavoritesCarouselView.swift
@@ -1,17 +1,47 @@
 import Boutique
 import SwiftUI
 
-// If you'd like to do more complex transforms on imagesController.images
-// it's worth remembering that you can subscribe to the @Published property your controller exposes.
+// Below you'll see code that looks like this.
+// CarouselView(
+//    items: self.images,
+//    contentView: { image in
+//      ...
+//    }
+// )
+//
+// In place of `self.images` you may be tempted to directly use `imagesController.images`.
+// That will likely be fine for views rendering a small amount of data, especially if you
+// read the data directly (as opposed to performing operations such as map, filter, sort, etc).
+// But as the data grows this will create a performance bottleneck.
+//
+// The reason for this bottleneck is related to SwiftUI, and how SwiftUI renders Views.
+// The body of a SwiftUI is recomputed often, and you don't know when they're going to recompute.
+// It can happen any time, but because SwiftUI Views are simple structs rendering and recomputing
+// is not computationally expensive. What that means though is that imagesController.images will
+// be recalculated every render, and that can be computationally expensive,
+// especially if you apply operations like map, filter, sort, or have a large data set.
+//
+// But if you store the result of imagesController.images in a `@State` var, it will no longer
+// be recomputed on every render. That's because `@State` provides reference semantics
+// to the var, `self.images, that allow `self.images` to be persisted across View renders.
+// That turns an expensive operation into minimal cost since there are
+// no longer any computations occurring on re-render.
+
+// That's a long way of saying if you're working with a large data set or would like
+// to do more complex transforms on imagesController.images it's worth remembering
+// that you can subscribe to the @Published property your controller exposes.
+// This approach is probably worth using as your default, especially if you
+// don't know how large your array will be.
 //
 // Here `ImagesController`'s `images` is a @Published property which you can store and manipulate
-// in your own `@State` property by subscribing to self.imagesController.$images like so.
+// in your own `@State` property by subscribing to self.imagesController.$images.$items like so.
 //
 //    @State private var images: [RemoteImage] = []
 //
-//    .onReceive(self.imagesController.$images, perform: {
+//    .onReceive(self.imagesController.$images.$items, perform: {
 //        self.images = $0.filter({ $0.width > 500 && $0.height > 500 })
 //    })
+
 
 /// A horizontally scrolling carousel that displays the red panda images a user has favorited.
 struct FavoritesCarouselView: View {
@@ -19,6 +49,7 @@ struct FavoritesCarouselView: View {
     @StateObject private var imagesController = ImagesController()
 
     @State private var animation: Animation? = nil
+    @State private var images: [RemoteImage] = []
 
     var body: some View {
         VStack {
@@ -55,7 +86,7 @@ struct FavoritesCarouselView: View {
             } else {
                 HStack {
                     CarouselView(
-                        items: self.imagesController.images.sorted(by: { $0.createdAt > $1.createdAt}),
+                        items: self.images,
                         contentView: { image in
                             ZStack(alignment: .topTrailing) {
                                 RemoteImageView(image: image)
@@ -87,6 +118,9 @@ struct FavoritesCarouselView: View {
                 })
             }
         }
+        .onReceive(self.imagesController.$images.$items, perform: {
+            self.images = $0.sorted(by: { $0.createdAt > $1.createdAt})
+        })
         .frame(height: 200.0)
         .background(Color.palette.background)
     }
